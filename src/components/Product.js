b686eae912569831424ba3d9289219981b7312b1
Sandip Sah <sandipsah1205@gmail.com>
2021-08-18 01:26:37 +0200

    adding some style

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,97 +9,31 @@ import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 export default function Product(props) {
   const { handleDetail, addToCart } = useProduct();
 
-  const { title, id,img,   priceRange} =
-    props.product;
-  const price = priceRange;
+  const { title, id, img, priceRange } = props.product;
 
   const defaultImg =
     "https://assets.hongkiat.com/uploads/famous-brands-make-unexpected-products/lipton-cigarette.jpg?newedit";
 
   return (
-    <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3 productWrapper">
-      <div className="card ">
-        <div
-          className="img-container p-5"
-          onClick={() => {
-            handleDetail(id);
-          }}
-        >
-          <Link to="/detail">
-            <img src={img||defaultImg} alt="img" width="50px" height="200px" className="card-img-top" />
-          </Link>
-          <IconButton
-            aria-label="delete"
-            className="cart-btn display-4"
-            onClick={() => {
-              addToCart(id);
-            }}
-          >
-            <AddShoppingCartIcon />
-          </IconButton>
-        </div>
-        <div className="card-footer d-flex justify-content-between">
-          <p className="align-self-center mb-0">{title}</p>
-          <h5 className="text-blue font-italic mb-0">
-            <span className="mr-1">$</span>
-            {price}
-          </h5>
-        </div>
-        
-      </div>
-    </ProductWrapper>
+    <div
+      className="col-4"
+      onClick={() => {
+        handleDetail(id);
+      }}
+    >
+      <Link to="/detail">
+        <img src={img} alt="img" />
+      </Link>
+      <h4>{title}</h4>
+      <p>{priceRange}</p>
+      <button
+        className="cart-button"
+        onClick={() => {
+          addToCart(id);
+        }}
+      >
+        <AddShoppingCartIcon />
+      </button>
+    </div>
   );
 }
-
-const ProductWrapper = styled.div`
-  .card {
-    border-color: transparent;
-    transition: all 1s linear;
-  }
-  .card-footer {
-    background: transparent;
-    border-top: transparent;
-    transition: all 1s linear;
-  }
-  &:hover {
-    .card {
-      border: 0.04rem solid rgba(0, 0, 0, 0.2);
-      box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
-      background: #d0d0d0;
-    }
-    .card-footer {
-      background: rgba(247, 247, 247);
-    }
-  }
-  .img-container {
-    position: relative;
-    overflow: hidden;
-  }
-  .card-img-top {
-    transition: all 1s linear;
-  }
-  .img-container:hover .card-img-top {
-    transform: scale(1.2);
-  }
-  .cart-btn {
-    position: absolute;
-    bottom: 0;
-    right: 0;
-    background: var(--lightBlue);
-    padding: 0.2rem 0.4rem;
-    border: none;
-    color: var(--mainWhite);
-    font-size: 1.7rem;
-    border-radius: 0.5rem 0 0 0;
-    transform: translate(100%, 100%);
-    transition: all 1s linear;
-  }
-  .img-container: hover .cart-btn {
-    transform: translate(0, 0);
-  }
-
-  .cart-btn:hover {
-    color: var(--mainBlue);
-    cursor: pointer;
-  }
-`;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,22 +2,23 @@ import React from "react";
 import Product from "./Product";
 import Title from "./Title";
 import { useProduct } from "./ProductContext";
+// import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 
 export default function ProductList() {
   const { publicProducts } = useProduct();
   return (
-    <div className="py-5">
-      <div className="container">
-        <Title name="Available" title="Request" />
-        <div className="row" style={{ margin: "auto" }}>
-          {publicProducts.map(
-            (eachProduct) =>
-              !eachProduct.addedToCart && (
-                <Product key={eachProduct.id} product={eachProduct} />
-              )
-          )}
-        </div>
+    <div className="small-container">
+      <h2>Available Requests</h2>
+      <div className="row">
+        {publicProducts.map(
+          (eachProduct) =>
+            !eachProduct.addedToCart && (
+              <Product key={eachProduct.id} product={eachProduct} />
+            )
+        )}
       </div>
     </div>
   );
+
+ 
 }
